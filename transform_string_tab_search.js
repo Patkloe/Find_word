4e244c71011481ecc3572aa165e5823e28cc70f0
Patkloe <52906039+Patkloe@users.noreply.github.com>
2021-04-21 08:05:59 -0500

    Create transform_string_tab_search.js

---
+++ transform_string_tab_search.js
@@ -0,0 +1,12 @@
+function transform_string_tab_search(s, v){
+  var tab = s.split(", ");   // criteria pour creer le tableau
+  var nbre = 0;
+ for(var i = 0; i < tab.length; i++){
+     if (tab[i] === v)
+         nbre++;
+ }
+  alert( v + 'found'  +  " " +  nbre  +  " "  +  'times');
+  return nbre;
+}
+var ex = 'aa, bb, cc, dd, aa';
+transform_string_tab_search('aa, bb, cc, dd, aa','aa');
